bd15904091375315b30a4e94e0b19373fa8c14f3
Matheus Pereira <tuliocorreamatheus@gmail.com>
2026-01-31 12:03:58 -0300

    feat: registra rota de CPF no app principal

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 
+const cpfRoutes = require('./routes/cpfRoutes');
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/cpf', cpfRoutes);
 
 app.get('/', (req, res) => {
   res.json({
