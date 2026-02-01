02b7947323f4cee3ae5fce8edf4021a2dd26e75f
Matheus Pereira <tuliocorreamatheus@gmail.com>
2026-02-01 14:10:24 -0300

    refactor: padroniza formato das respostas da API

--- src/controllers/cpfController.js
+++ src/controllers/cpfController.js
@@ -3,25 +3,46 @@ const { validarCPF } = require('../services/cpfService');
 function validar(req, res) {
   const { cpf } = req.query;
 
+  // CPF não informado
   if (!cpf) {
     return res.status(400).json({
       success: false,
       error: {
+        code: 'CPF_REQUIRED',
         message: 'CPF não informado'
       }
     });
   }
 
-  const cpfLimpo = cpf.replace(/\D/g, '');
-  const valido = validarCPF(cpfLimpo);
+  // CPF com caracteres inválidos
+  if (!/^[\d.\-]+$/.test(cpf)) {
+    return res.status(400).json({
+      success: false,
+      error: {
+        code: 'CPF_FORMAT_INVALID',
+        message: 'CPF contém caracteres inválidos'
+      }
+    });
+  }
+
+  const resultado = validarCPF(cpf);
+
+  if (!resultado.valid) {
+    return res.status(422).json({
+      success: false,
+      error: resultado.error
+    });
+  }
 
   return res.json({
     success: true,
     data: {
-      cpf: cpfLimpo,
-      valid: valido
+      cpf: resultado.cpf,
+      valid: true
     }
   });
 }
 
-module.exports = { validar };
+module.exports = {
+  validar
+};
