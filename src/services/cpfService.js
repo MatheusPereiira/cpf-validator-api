cce2765b92924901eec357e8902a89c0fb1f8589
Matheus Pereira <tuliocorreamatheus@gmail.com>
2026-02-01 14:09:32 -0300

    refactor: melhora tratamento de erros na validação de CPF

--- src/services/cpfService.js
+++ src/services/cpfService.js
@@ -1,32 +1,72 @@
 function validarCPF(cpf) {
-  if (!cpf) return false;
+  // Remove tudo que não for número
+  const cpfLimpo = cpf.replace(/\D/g, '');
 
-  // remove pontos e traços
-  cpf = cpf.replace(/\D/g, '');
+  // CPF deve ter 11 dígitos
+  if (cpfLimpo.length !== 11) {
+    return {
+      valid: false,
+      error: {
+        code: 'CPF_LENGTH_INVALID',
+        message: 'CPF deve conter 11 dígitos'
+      }
+    };
+  }
 
-  if (cpf.length !== 11) return false;
-  if (/^(\d)\1{10}$/.test(cpf)) return false;
+  // Bloqueia CPFs com todos os dígitos iguais
+  if (/^(\d)\1{10}$/.test(cpfLimpo)) {
+    return {
+      valid: false,
+      error: {
+        code: 'CPF_INVALID',
+        message: 'CPF informado é inválido'
+      }
+    };
+  }
 
+  // Validação matemática
   let soma = 0;
   let resto;
 
   for (let i = 1; i <= 9; i++) {
-    soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+    soma += parseInt(cpfLimpo.substring(i - 1, i)) * (11 - i);
   }
 
   resto = (soma * 10) % 11;
   if (resto === 10 || resto === 11) resto = 0;
-  if (resto !== parseInt(cpf.substring(9, 10))) return false;
+  if (resto !== parseInt(cpfLimpo.substring(9, 10))) {
+    return {
+      valid: false,
+      error: {
+        code: 'CPF_INVALID',
+        message: 'CPF informado é inválido'
+      }
+    };
+  }
 
   soma = 0;
   for (let i = 1; i <= 10; i++) {
-    soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+    soma += parseInt(cpfLimpo.substring(i - 1, i)) * (12 - i);
   }
 
   resto = (soma * 10) % 11;
   if (resto === 10 || resto === 11) resto = 0;
+  if (resto !== parseInt(cpfLimpo.substring(10, 11))) {
+    return {
+      valid: false,
+      error: {
+        code: 'CPF_INVALID',
+        message: 'CPF informado é inválido'
+      }
+    };
+  }
 
-  return resto === parseInt(cpf.substring(10, 11));
+  return {
+    valid: true,
+    cpf: cpfLimpo
+  };
 }
 
-module.exports = { validarCPF };
+module.exports = {
+  validarCPF
+};
