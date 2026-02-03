ef1a9e187d2e7ef742a000aaf2ee6b0903617b40
Matheus Pereira <tuliocorreamatheus@gmail.com>
2026-02-03 00:03:42 -0300

    chore: registra middleware global de erros no app

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const cors = require('cors');
 
 const cpfRoutes = require('./routes/cpfRoutes');
+const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// Rotas
 app.use('/cpf', cpfRoutes);
 
 app.get('/', (req, res) => {
@@ -16,5 +18,7 @@ app.get('/', (req, res) => {
     message: 'CPF Validator API online'
   });
 });
+
+app.use(errorHandler);
 
 module.exports = app;
