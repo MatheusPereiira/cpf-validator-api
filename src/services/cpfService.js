178cc66dcabb839bbc68e39e7707b01e9cb3805c
Matheus Pereira <tuliocorreamatheus@gmail.com>
2026-02-04 11:18:02 -0300

    refactor: melhora lógica de validação de CPF no service

--- src/services/cpfService.js
+++ src/services/cpfService.js
@@ -1,70 +1,38 @@
 function validarCPF(cpf) {
-  // Remove tudo que não for número
-  const cpfLimpo = cpf.replace(/\D/g, '');
-
-  // CPF deve ter 11 dígitos
-  if (cpfLimpo.length !== 11) {
-    return {
-      valid: false,
-      error: {
-        code: 'CPF_LENGTH_INVALID',
-        message: 'CPF deve conter 11 dígitos'
-      }
-    };
-  }
-
   // Bloqueia CPFs com todos os dígitos iguais
-  if (/^(\d)\1{10}$/.test(cpfLimpo)) {
-    return {
-      valid: false,
-      error: {
-        code: 'CPF_INVALID',
-        message: 'CPF informado é inválido'
-      }
-    };
+  if (/^(\d)\1{10}$/.test(cpf)) {
+    return false;
   }
 
-  // Validação matemática
   let soma = 0;
   let resto;
 
+  // Primeiro dígito verificador
   for (let i = 1; i <= 9; i++) {
-    soma += parseInt(cpfLimpo.substring(i - 1, i)) * (11 - i);
+    soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
   }
 
   resto = (soma * 10) % 11;
   if (resto === 10 || resto === 11) resto = 0;
-  if (resto !== parseInt(cpfLimpo.substring(9, 10))) {
-    return {
-      valid: false,
-      error: {
-        code: 'CPF_INVALID',
-        message: 'CPF informado é inválido'
-      }
-    };
+
+  if (resto !== parseInt(cpf.substring(9, 10))) {
+    return false;
   }
 
+  // Segundo dígito verificador
   soma = 0;
   for (let i = 1; i <= 10; i++) {
-    soma += parseInt(cpfLimpo.substring(i - 1, i)) * (12 - i);
+    soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);
   }
 
   resto = (soma * 10) % 11;
   if (resto === 10 || resto === 11) resto = 0;
-  if (resto !== parseInt(cpfLimpo.substring(10, 11))) {
-    return {
-      valid: false,
-      error: {
-        code: 'CPF_INVALID',
-        message: 'CPF informado é inválido'
-      }
-    };
+
+  if (resto !== parseInt(cpf.substring(10, 11))) {
+    return false;
   }
 
-  return {
-    valid: true,
-    cpf: cpfLimpo
-  };
+  return true;
 }
 
 module.exports = {
