d521d79f83d7fe5b4a365b1822b95f57daf64f1c
Matheus Pereira <tuliocorreamatheus@gmail.com>
2026-02-07 23:58:07 -0300

    refactor: aprimora errorHandler com suporte a ambientes

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,4 +1,7 @@
 module.exports = (err, req, res, next) => {
+  const isDev = process.env.NODE_ENV === 'development';
+
+  // Erro esperado (AppError)
   if (err.isOperational) {
     return res.status(err.statusCode).json({
       success: false,
@@ -10,6 +13,20 @@ module.exports = (err, req, res, next) => {
   }
 
   // Erro inesperado (bug)
+  if (isDev) {
+    console.error('Erro inesperado:', err);
+
+    return res.status(500).json({
+      success: false,
+      error: {
+        code: 'INTERNAL_SERVER_ERROR',
+        message: err.message,
+        stack: err.stack
+      }
+    });
+  }
+
+  // Produção: resposta limpa
   console.error(err);
 
   return res.status(500).json({
